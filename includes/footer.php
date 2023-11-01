d3d7217e700413dd1de466e80be4e344c636ddca
musaahmedmusa <146739469+musaahmedmusa@users.noreply.github.com>
2023-11-01 13:04:48 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+&copy; <?php echo date("Y"); ?> - Al-Qalam University Hostel Management System - Developed by Musa Ahmed Musa <a href="https://codeastro.com"></a>
+</footer>
